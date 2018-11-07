18161f1e82813721b4ac4455d0ecc01ac69b0e52
airibot <airigare@mikmak.cc>
2018-11-07 02:07:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# CF Sample App Node.js
+
+A sample [Express](http://expressjs.com/) application to deploy to Cloud Foundry which works out of the box.
+
+## Run locally
+
+1. Install [Node.js and npm](https://nodejs.org/)
+1. Run `npm install`
+1. Run `npm start`
+1. Visit [http://localhost:3000](http://localhost:3000)
+
+## Run in the cloud
+
+1. Install the [cf CLI](https://github.com/cloudfoundry/cli#downloads)
+1. Run `cf push airigare-api -m 128M -n airigare-api -u none`
+1. Visit the given URL
---
+++ src/index.js
@@ -0,0 +1,66 @@
+	var express = require('express');
+	var path = require('path');
+	var mysql = require('mysql');
+	var app = express();
+
+	const ConfigParser = require('configparser');
+ 	const config = new ConfigParser();
+ 	config.read('.config.ini');
+
+ 	var host = config.get('DB', 'host');
+ 	var user = config.get('DB', 'user');
+ 	var password = config.get('DB', 'password');
+
+	var pumpTime = 120;
+	var sleepIterations = 3380;
+
+	var connection = mysql.createConnection({
+		host     : host,
+		user     : user,
+		password : password
+	});
+
+
+
+	app.set('port', 3000);
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'jade');
+	app.use(express.static(path.join(__dirname, 'public')));
+
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
+
+	app.get('/mikmakAPI/airigare/weatherDW/last24hours', function(req, res){
+		connection.query('USE weatherDW');
+		connection.query('SELECT * FROM actualWeater', function(err, data){
+			res.json(data);
+		});
+	});
+
+	app.get('/mikmakAPI/airigare/Station/Status', function(req, res){
+		console.log("/mikmakAPI/airigare/Station/Status");
+		var api = req.query.API;
+		var id = req.query.id;
+		var value = req.query.value;
+		connection.query('USE iRig');
+		var query = 'CALL writeLog("' + api + '", ' + id + ', ' + value + ')'
+		connection.query(query, function(err, data){
+			res.json(data);
+		});
+	});
+
+	app.get('/mikmakAPI/airigare/Station/getInstructions', function(req, res){
+		console.log("/mikmakAPI/airigare/Station/getInstructions/");
+		var api = req.query.API;
+		console.log(api);
+		connection.query('USE iRig');
+		connection.query('SELECT * FROM `vInstructions` where `sysID` = "' + api + '"', function(err, data){
+			res.json(data[0]);
+		});
+	});
+	
+	app.listen(process.env.PORT || app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
